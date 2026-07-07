d2764c3bb490d7018487860e807f92818755f4d4
kaushamb tiwari <kaushambtiwari@gmail.com>
2026-07-07 21:07:27 +0530

    added db connection config connected db successfully

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,13 +1,17 @@
-const express= require("express");
+const express = require("express");
 const router = express.Router();
-const {getContacts,getContact,createContact,updateContact,deleteContact}=require("../controllers/contactController")
+const {
+  getContacts,
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
+} = require("../controllers/contactController");
 
-router.route("/").get(getContacts)
-router.route("/:id").get(getContact)
-router.route("/:id").post(createContact)
-router.route("/:id").put(updateContact)
-router.route("/:id").delete(deleteContact)
-
-
+router.route("/").get(getContacts);
+router.route("/:id").get(getContact);
+router.route("/:id").post(createContact);
+router.route("/:id").put(updateContact);
+router.route("/:id").delete(deleteContact);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express= require('express');
 const dotenv = require('dotenv').config({quiet : true});
-const errorHandler = require("./middleware/errorHandler")
+const errorHandler = require("./middleware/errorHandler");
+const connectDb = require('./config/dbConnection');
 
+connectDb();
 const app= express();
 
 const port = process.env.PORT || 5000;
